1012a4889b0d142eb0cbe3c5dbeef7ba657540e5
Spiros G. <102389520+spiros-g@users.noreply.github.com>
2026-09-14 06:32:16 +0300

    Use validated vial PNG resource

--- app/src/main/java/gr/peptidetracker/app/ui/GlassUi.kt
+++ app/src/main/java/gr/peptidetracker/app/ui/GlassUi.kt
@@ -308,7 +308,7 @@ fun StoreVialImage(
         )
 
         Image(
-            painter = painterResource(R.drawable.peptide_vial),
+            painter = painterResource(R.drawable.peptide_vial_clean),
             contentDescription = contentDescription,
             contentScale = ContentScale.Fit,
             modifier = Modifier.fillMaxSize()
