abcdddf8ed797f4678e60619785d153da2e6a571
Spiros G. <102389520+spiros-g@users.noreply.github.com>
2026-09-14 05:26:25 +0300

    Bump app to v2.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "gr.peptidetracker.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.0.0"
+        versionCode = 6
+        versionName = "2.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
