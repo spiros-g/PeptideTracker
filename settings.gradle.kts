ad20dbfea275157da518840a9573bec1c4a11963
Spiros G. <102389520+spiros-g@users.noreply.github.com>
2026-09-18 18:42:45 +0300

    Rename Gradle project to PeptideTracker

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,5 @@ dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories { google(); mavenCentral() }
 }
-rootProject.name = "PeptideTrackerGR"
+rootProject.name = "PeptideTracker"
 include(":app")
