09bfc76e0db577506b39405782acfc7604e7e9b9
Spiros G. <102389520+spiros-g@users.noreply.github.com>
2026-09-18 19:35:35 +0300

    Fix Settings padding import

--- app/src/main/java/gr/peptidetracker/app/ui/screens/SettingsScreen.kt
+++ app/src/main/java/gr/peptidetracker/app/ui/screens/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
